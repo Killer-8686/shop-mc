e8a7ce7608846989773df54c2e9e9c1437374658
Killer-8686 <November-2020>
2022-03-23 17:19:29 +0500

    Add instance ID for testing Lad Balancer. Add @Value for controller

--- eureka-client/src/main/java/ru/masta/client/eurekaclient/ExampleController.java
+++ eureka-client/src/main/java/ru/masta/client/eurekaclient/ExampleController.java
@@ -1,5 +1,6 @@
 package ru.masta.client.eurekaclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/main")
 public class ExampleController {
 
+    @Value("${eureka.instance.instance-id}")
+    private String id;
+
     @GetMapping("/test")
     public String get(){
-        return "test string";
+        return "test string from " + id;
     }
 }

32ff5504e8e9327c9c524e684c7e3fcfd4f3b59e
Killer-8686 <November-2020>
2022-04-25 10:02:56 +0500

    Add utils-module

---
+++ utils-module/src/main/java/ru/masta/utils/utilsmodule/UtilsModuleApplication.java
@@ -0,0 +1,13 @@
+package ru.masta.utils.utilsmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilsModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtilsModuleApplication.class, args);
+    }
+
+}
